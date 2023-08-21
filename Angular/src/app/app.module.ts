fab62689a2a660d4da99052f3d81b8786c667fbe
Erecutoro <alex.li@epitech.eu>
2023-08-21 11:26:40 +0200

    Adding general home page structure

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './profile/about/about.component';
+import { EducationComponent } from './profile/education/education.component';
+import { HobbyComponent } from './profile/hobby/hobby.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    AboutComponent,
+    EducationComponent,
+    HobbyComponent
   ],
   imports: [
     BrowserModule,
